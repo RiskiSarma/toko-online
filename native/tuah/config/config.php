8afc823263b5d3393ccd615195749334d0f86d44
RiskiSarma <riski.sarma28@gmail.com>
2023-11-27 10:27:35 +0700

    tes

---
+++ native/tuah/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/*----- Kofigurasi Database -----*/
+define('hostname', '127.0.0.1');
+define('username', 'root');
+define('password', '');
+define('database', 'toko_online');
+
+/*----- Url -----*/
+define('url', 'http://localhost/workshop/toko_online/native/tuah/');
+
+/*----- Author -----*/
+define('author', 'Riski Sarma');
